7e09e8440ad5d121fbb251c79b94dd6d531b526a
KDuyHero <dtrang1570@gmail.com>
2022-11-06 18:12:41 +0700

    state and output List value

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
-
+import {useState} from 'react'
 
 const App = () => {
-  // Khai báo biến
-  let obj = {
-    name: "Duy", 
-    year: "2022",
+
+  let [name, setName] = useState('Duy')     // name is string
+  let [address, setAddress] = useState('')  // address is string
+  let [course, setCourse] = useState([      // course is a array of object
+    {id: 1, name: "learn HTML"}, 
+    {id: 2, name: "learn CSS"}, 
+    {id: 3, name: "learn js and react"}
+  ])
+  const handleEventClick = (event) => {
+    let newCourse = {id: 4, name: address}
+    setCourse([...course, newCourse])
+    setAddress('')
+  }
+
+  const handleEventTextInput = (event) => {
+    setAddress(event.target.value)
   }
 
   return (
@@ -15,8 +27,13 @@ const App = () => {
       <Nav></Nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p style = {{color: 'red'}}>
-          This is react Project
+        <ul>
+          {course.map(course=> {
+             return <li key={course.id}>{course.name}</li>
+          })}
+        </ul>
+        <p style={{ color: 'red' }}>
+          This is react Project {name}
         </p>
         <a
           className="App-link"
@@ -26,6 +43,8 @@ const App = () => {
         >
           Learn React
         </a>
+        <input type="text" value ={address} onChange={(event) => handleEventTextInput(event)}/>
+        <button type="button" onClick={(event) => handleEventClick(event)}>click me to refresh</button>
       </header>
     </div>
   );
